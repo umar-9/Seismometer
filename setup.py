678539180762eafa75af968b7d880e6addc487c2
usultonov-b18 <108488645+usultonov-b18@users.noreply.github.com>
2023-07-11 19:26:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
-sudo pip install Adafruit-Blinka
-sudo pip install adafruit-circuitpython-ads1x15
+# Terminal equivalent commands:
+# sudo pip install Adafruit-Blinka
+# sudo pip install adafruit-circuitpython-ads1x15
+
+from setuptools import setup, find_packages
+
+setup(
+  name = 'Adafruit-Blinka'
+  version = '8.20.0'
+  packages = find_packages()
+)
+
+setup(
+  name = 'adafruit-circuitpython-ads1x15'
+  version = '2.2.22'
+  packages = find_packages()
+)
