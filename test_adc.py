f3ecb0388ff832c73f3554c0e9f4da0582f4df24
usultonov-b18 <108488645+usultonov-b18@users.noreply.github.com>
2023-07-11 21:24:41 +0100

    Update and rename test.py to test_adc.py

--- test_adc.py
+++ test_adc.py
@@ -1,17 +1,26 @@
 # Testing
 # Ensure to enable I2C on Pi
-
+import time
 import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+
 # Uncomment in real system - test voltages first
 # ads.gain = 16 # Supposed to be 100 minimum according to the instruction manual, but only 2/3, 1, 2, 4, 8, 16 available
 
-i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1015(i2c)
-chan = AnalogIn(ads, ADS.P0) # Change pin
+print("{:>5}\t{:>5}".format('raw', 'v'))
 
 while True:
-  print(chan.value, chan.voltage) # 12 bit (supposed to be 16 bit)
+    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    time.sleep(0.5)
+    
